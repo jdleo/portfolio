b5120eaabea5f25dbc9faef4e421d5e478f639c2
John Leonardo <jdleonardo5@icloud.com>
2018-07-30 14:16:31 -0700

    refactored state components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Projects from '../components/Projects';
+import Tech from '../components/Tech';
+import Home from '../components/Home';
 
-export default () => {
-  return (
-    <Layout>
-      
-    </Layout>
-  );
-};
+class Main extends React.Component {
+
+  state = {
+    currentPage: 'home'
+  }
+
+  render () {
+
+    const pages = {
+      home: (
+        <Home/>
+      ),
+      about: (
+        <About/>
+      ),
+      projects: (
+        <Projects/>
+      ),
+      tech: (
+        <Tech/>
+      ),
+      contact: (
+        <Contact/>
+      )
+    };
+
+    return (
+      <Layout>
+        {pages[this.state.currentPage]}
+      </Layout>
+    );
+  }
+}
+
+export default Main;
