3d4cd33992384c4b535676d0e17592096cacb750
John Leonardo <jdleonardo5@icloud.com>
2018-07-30 16:34:27 -0700

    remove comment action

--- components/About.js
+++ components/About.js
@@ -130,9 +130,6 @@ export default () => {
 
                   </Image.Group>
                 </Comment.Text>
-                <Comment.Actions>
-                  <Comment.Action>Reply</Comment.Action>
-                </Comment.Actions>
               </Comment.Content>
             </Comment>
           </Comment.Group>
