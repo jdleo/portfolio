1578ce9eb317121b496de4f6754df8cccac6dd7c
John Leonardo <jdleonardo5@icloud.com>
2018-07-30 14:02:56 -0700

    styled header

--- components/Header.js
+++ components/Header.js
@@ -5,26 +5,31 @@ import {Image, Button, Segment, Card} from 'semantic-ui-react';
 export default () => {
   return (
     <div>
-      <Card.Group centered>
-        <Image src='/images/wireframe/square-image.png' size='medium' circular />  
+      <Card.Group centered style={{'marginTop': '60px'}}>
+        <Image
+          src='https://raw.githubusercontent.com/jdleo/portfolio/master/assets/me.JPG'
+          circular
+          style={{'width': '200px', 'height':'200px'}}
+        />
       </Card.Group>
 
-
-      <Button inverted>
-        Home
-      </Button>
-      <Button inverted color='red'>
-        About Me
-      </Button>
-      <Button inverted color='blue'>
-        Projects
-      </Button>
-      <Button inverted color='yellow'>
-        Tech
-      </Button>
-      <Button inverted color='green'>
-        Contact
-      </Button>
+      <Card.Group centered style={{'marginTop':'40px'}}>
+        <Button inverted>
+          Home
+        </Button>
+        <Button inverted color='red'>
+          About Me
+        </Button>
+        <Button inverted color='blue'>
+          Projects
+        </Button>
+        <Button inverted color='yellow'>
+          Tech
+        </Button>
+        <Button inverted color='green'>
+          Contact
+        </Button>
+      </Card.Group>
     </div>
   );
 };
