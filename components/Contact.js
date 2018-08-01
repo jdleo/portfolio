b52c6afd658c22f04e0d1d0d4881caeae7227dda
John Leonardo <jdleonardo5@icloud.com>
2018-07-31 20:26:56 -0700

    tech

--- components/Contact.js
+++ components/Contact.js
@@ -1,23 +1,86 @@
 import React from 'react';
 import {Container, Divider, Button, Form, Segment, TextArea} from 'semantic-ui-react';
 
+class Contact extends React.Component {
 
-export default () => {
-  return (
-    <Container style={{'marginTop':'40px'}}>
-      <Divider horizontal inverted>
-        Contact
-      </Divider>
-      <Segment inverted>
-        <Form inverted>
-          <Form.Group widths='equal'>
-            <Form.Input fluid label='Email address' placeholder='Email address' />
-            <Form.Input fluid label='Subject' placeholder='Subject' />
-          </Form.Group>
-          <TextArea autoHeight placeholder='What do you wanna say?' rows={3} style={{'marginTop':'40px'}} />
-          <Button type='submit' style={{'marginTop':'40px'}}>Submit</Button>
-        </Form>
-      </Segment>
-    </Container>
-  );
-};
+  state = {
+    'email': '',
+    'subject': '',
+    'body': ''
+  }
+
+  handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+   onSubmit = (e) => {
+       e.preventDefault();
+
+       var http = require("http");
+
+        var options = {
+          hostname: 'https://api.mailjet.com',
+          port: 80,
+          path: '/v3/send',
+          method: 'POST',
+          auth: '68c3c366255c769c4ca3ecfc952d41c1:ec763208675bb6ee27f57d1ab777f203',
+          headers: {
+              'Content-Type': 'application/json',
+          }
+        };
+        var req = http.request(options, function(res) {
+          console.log('Status: ' + res.statusCode);
+          console.log('Headers: ' + JSON.stringify(res.headers));
+          res.setEncoding('utf8');
+          res.on('data', function (body) {
+            console.log('Body: ' + body);
+          });
+        });
+        req.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+        });
+        // write data to request body
+        req.write(
+          `{"FromEmail": "pilot@mailjet.com", "FromName":"Mailjet Pilot", "Subject":"${this.state.subject}", "Text-part":"${this.state.body}", Recipients: [{"Email":"yo@jdleo.me"}]}`
+        );
+        req.end();
+
+       this.setState({
+            email: '',
+            subject: '',
+            body: ''
+       })
+    }
+
+  render() {
+    return(
+      <Container style={{'marginTop':'40px'}}>
+        <Divider horizontal inverted>
+          Contact
+        </Divider>
+        <Segment inverted>
+          <Form inverted>
+            <Form.Group widths='equal'>
+              <Form.Input fluid label='Email address' placeholder='Email address' value={this.state.email} name='email' onChange={e => this.handleChange(e)}/>
+            <Form.Input fluid label='Subject' placeholder='Subject'  value={this.state.subject} name='subject' onChange={e => this.handleChange(e)}/>
+            </Form.Group>
+            <TextArea
+              autoHeight
+              placeholder='What do you wanna say?'
+              rows={3}
+              style={{'marginTop':'40px'}}
+              value={this.state.body}
+              name='body'
+              onChange={e => this.handleChange(e)}
+            />
+            <Button type='submit' style={{'marginTop':'40px'}} onClick={(e) => this.onSubmit(e)}>Submit</Button>
+          </Form>
+        </Segment>
+      </Container>
+    );
+  }
+}
+
+export default Contact;
--- components/Tech.js
+++ components/Tech.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Divider} from 'semantic-ui-react';
+import {Container, Divider, Segment, Header, Label, Card} from 'semantic-ui-react';
 
 
 export default () => {
@@ -8,6 +8,79 @@ export default () => {
       <Divider horizontal inverted>
         Tech
       </Divider>
+
+      <Segment raised>
+        <Header as='h3' textAlign='center'>
+          Languages
+        </Header>
+        <Card.Group centered style={{'marginTop':'20px'}}>
+          <Label.Group size='big'>
+            <Label color ='orange'>Java</Label>
+            <Label color ='blue'>Python</Label>
+            <Label color ='black'>HTML</Label>
+            <Label color ='yellow'>Javascript</Label>
+            <Label color ='orange'>Swift</Label>
+            <Label color ='grey'>R</Label>
+            <Label color ='violet'>Kotlin</Label>
+            <Label color ='blue'>C</Label>
+            <Label color ='black'>Solidity</Label>
+          </Label.Group>
+        </Card.Group>
+      </Segment>
+
+      <Segment raised>
+        <Header as='h3' textAlign='center'>
+          Frameworks / Libraries
+        </Header>
+
+        <Card.Group centered style={{'marginTop':'20px'}}>
+          <Label.Group size='big'>
+            <Label color ='teal'>React</Label>
+            <Label color ='green'>Node</Label>
+            <Label color ='grey'>Express</Label>
+            <Label color ='orange'>Next</Label>
+            <Label color ='teal'>Semantic</Label>
+            <Label color ='purple'>MDL</Label>
+            <Label color ='brown'>Mocha</Label>
+            <Label color ='yellow'>Bootstrap</Label>
+            <Label color ='black'>Mongodb</Label>
+          </Label.Group>
+        </Card.Group>
+      </Segment>
+
+      <Segment raised>
+        <Header as='h3' textAlign='center'>
+          Cloud / Devops
+        </Header>
+
+        <Card.Group centered style={{'marginTop':'20px'}}>
+          <Label.Group size='big'>
+            <Label color ='purple'>Heroku</Label>
+            <Label color ='blue'>Digital Ocean</Label>
+            <Label color ='grey'>AWS</Label>
+            <Label color ='yellow'>Firebase</Label>
+            <Label color ='teal'>Firestore</Label>
+            <Label color ='violet'>Google Cloud</Label>
+            <Label color ='black'>Linux</Label>
+          </Label.Group>
+        </Card.Group>
+      </Segment>
+
+      <Segment raised>
+        <Header as='h3' textAlign='center'>
+          Editors
+        </Header>
+        <Card.Group centered style={{'marginTop':'20px'}}>
+          <Label.Group size='big'>
+            <Label color ='violet'>Brackets</Label>
+            <Label color ='green'>Atom</Label>
+            <Label color ='blue'>Xcode</Label>
+            <Label color ='purple'>IntelliJ</Label>
+            <Label color ='black'>Vim</Label>
+          </Label.Group>
+        </Card.Group>
+      </Segment>
+
     </Container>
   );
 };
