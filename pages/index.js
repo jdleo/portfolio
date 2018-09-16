a3015c2ff31dc58eae4ed2dcb7752d1e241fc47d
John Leonardo <jdleonardo5@icloud.com>
2018-09-16 11:42:01 -0700

    event tracking in GA

--- pages/index.js
+++ pages/index.js
@@ -43,19 +43,49 @@ class Main extends React.Component {
     return (
       <Layout>
         <Card.Group centered style={{'marginTop':'40px'}}>
-          <Button inverted onClick={() => {this.setState({'currentPage':'home'})}}>
+          <Button inverted onClick={() => {
+              ReactGA.event({
+                category: 'User',
+                action: 'Switched to Home page'
+              });
+              this.setState({'currentPage':'home'})
+            }}>
             Home
           </Button>
-          <Button inverted color='red' onClick={() => {this.setState({'currentPage':'about'})}}>
+          <Button inverted color='red' onClick={() => {
+              ReactGA.event({
+                category: 'User',
+                action: 'Switched to About page'
+              });
+              this.setState({'currentPage':'about'})
+            }}>
             About Me
           </Button>
-          <Button inverted color='blue' onClick={() => {this.setState({'currentPage':'projects'})}}>
+          <Button inverted color='blue' onClick={() => {
+              ReactGA.event({
+                category: 'User',
+                action: 'Switched to Projects page'
+              });
+              this.setState({'currentPage':'projects'})
+            }}>
             Projects
           </Button>
-          <Button inverted color='yellow' onClick={() => {this.setState({'currentPage':'tech'})}}>
+          <Button inverted color='yellow' onClick={() => {
+              ReactGA.event({
+                category: 'User',
+                action: 'Switched to Tech page'
+              });
+              this.setState({'currentPage':'tech'})
+            }}>
             Tech
           </Button>
-          <Button inverted color='green' onClick={() => {this.setState({'currentPage':'contact'})}}>
+          <Button inverted color='green' onClick={() => {
+              ReactGA.event({
+                category: 'User',
+                action: 'Switched to Contact page'
+              });
+              this.setState({'currentPage':'contact'})
+            }}>
             Contact
           </Button>
         </Card.Group>
